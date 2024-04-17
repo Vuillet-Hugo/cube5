5f4a354fc93a8dde81b8c4d1680647a2239e95d5
Harrison Claudel <harrisonclaudel@MacBook-Air-de-Harrison.local>
2024-04-17 19:32:11 +0200

    maj

--- src/components/backoffice/content/listModules/popup/popupdelete/popupdelete.component.tsx
+++ src/components/backoffice/content/listModules/popup/popupdelete/popupdelete.component.tsx
@@ -4,7 +4,6 @@ import axios from "axios";
 // interfaces :
 import { IFormModule } from "../../../content.component";
 import LoaderComponent from "@/components/shared/loader/loader.component";
-
 // css :
 import "./popupdelete.style.css";
 
--- src/components/backoffice/content/listQualifications/popup/popupdelete/popupdelete.component.tsx
+++ src/components/backoffice/content/listQualifications/popup/popupdelete/popupdelete.component.tsx
@@ -4,7 +4,6 @@ import axios from "axios";
 // interfaces :
 import { IFormQualification } from "../../../content.component";
 import LoaderComponent from "@/components/shared/loader/loader.component";
-
 // css :
 import "./popupdelete.style.css";
 
